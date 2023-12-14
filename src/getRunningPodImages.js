48ce8d642c52221fae3c60119c49a49a8a19dc8c
urmas-villem <58630256+urmas-villem@users.noreply.github.com>
2023-12-14 14:17:27 +0200

    Shorten the curl commands and remove the github api version specification

--- src/getRunningPodImages.js
+++ src/getRunningPodImages.js
@@ -7,8 +7,8 @@ kubeConfig.loadFromDefault();
 const coreV1Api = kubeConfig.makeApiClient(k8s.CoreV1Api);
 
 const softwareCommands = {
-  'prometheus': `curl -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/prometheus/prometheus/releases/latest" | jq -r '.tag_name'`,
-  'alertmanager': `curl -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/prometheus/alertmanager/releases/latest" | jq -r '.tag_name'`,
+  'prometheus': `curl -s "https://api.github.com/repos/prometheus/prometheus/releases/latest" | jq -r '.tag_name'`,
+  'alertmanager': `curl -s "https://api.github.com/repos/prometheus/alertmanager/releases/latest" | jq -r '.tag_name'`,
 };
 
 async function fetchLatestImageTag(command) {
