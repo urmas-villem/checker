41ef3f45bfeed1a689e723890cbf2b9bc773dd22
urmas-villem <58630256+urmas-villem@users.noreply.github.com>
2023-12-15 13:43:15 +0200

    Reworked some things

--- src/getRunningPodImages.js
+++ src/getRunningPodImages.js
@@ -1,16 +1,11 @@
 const k8s = require('@kubernetes/client-node');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-const notes = require('./notes.json');
+const softwares = require('./software.json');
 
 const kubeConfig = new k8s.KubeConfig();
 kubeConfig.loadFromDefault();
 const coreV1Api = kubeConfig.makeApiClient(k8s.CoreV1Api);
-
-const softwareCommands = {
-  'prometheus': `curl -s "https://api.github.com/repos/prometheus/prometheus/releases/latest" | jq -r '.tag_name'`,
-  'alertmanager': `curl -s "https://api.github.com/repos/prometheus/alertmanager/releases/latest" | jq -r '.tag_name'`,
-};
 
 async function fetchLatestImageTag(command) {
   const networkErrorMessage = 'Network error occurred with getting latest version, try again in a few minutes';
@@ -37,16 +32,16 @@ async function getRunningPodImages() {
     const res = await coreV1Api.listPodForAllNamespaces();
     const containerObjects = res.body.items.flatMap(pod => {
       const appName = pod.metadata.labels?.app;
-      const command = softwareCommands[appName];
+      const software = softwares.find(s => s.name === appName);
 
-      if (command && pod.status.containerStatuses) {
+      if (software && pod.status.containerStatuses) {
         return pod.status.containerStatuses.map(status => ({
           containerName: status.name,
           imageRepository: status.image.split(':')[0],
           imageVersionUsedInCluster: status.image.split(':')[1],
           appName: appName,
-          command: command,
-          note: notes[appName] || ''
+          command: software.command,
+          note: software.note || ''
         }));
       }
       return [];
